44504d6e103488f1cdbd91aef0d8dd4eacdb753f
Francisco Lee <Hyong.joon.lee@ocsbstudent.ca>
2017-09-13 14:51:20 -0400

    Initial

---
+++ hello_world_GUI.py
@@ -0,0 +1,10 @@
+# Created by: Francisco Lee
+# Created on : 12-sep-2017
+# Created for: ICS3U
+# Daily assignment-Unit0-02
+#This program displays Hello World but in GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
